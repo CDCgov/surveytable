ea50fb7d0a3c6e264292318e2936f1d780ffb1a1
Alex Strashny <kpr9@cdc.gov>
2023-11-08 13:04:34 -0500

    var_case

---
+++ R/var_case.R
@@ -0,0 +1,37 @@
+#' Convert factor to logical
+#'
+#' @param newvr   name of the new logical variable to be created
+#' @param vr factor variable
+#' @param cases one or more levels of `vr` that are converted to `TRUE`. All other levels are converted to `FALSE`.
+#'
+#' @return (Nothing.)
+#' @family variables
+#' @export
+#'
+#' @examples
+#' set_survey("vars2019")
+#' var_case("Preventive care visits", "MAJOR", "Preventive care")
+#' tab("Preventive care visits")
+#' var_case("Surgery-related visits"
+#' , "MAJOR"
+#' , c("Pre-surgery", "Post-surgery"))
+#' tab("Surgery-related visits")
+var_case = function(newvr, vr, cases) {
+  design = .load_survey()
+  nm = names(design$variables)
+  assert_that(vr %in% nm, msg = paste("Variable", vr, "not in the data."))
+  assert_that(is.factor(design$variables[,vr])
+          , msg = paste0(vr, ": must be a factor. Is ", class(design$variables[,vr])[1] ))
+  assert_that(all(cases %in% levels(design$variables[,vr]) )
+            , msg = paste0(vr, ": at least some of the specified levels do not exist."))
+
+  if(newvr %in% nm) {
+    warning(newvr, ": overwriting a variable that already exists.")
+  }
+
+  design$variables[,newvr] = FALSE
+  idx = which(design$variables[,vr] %in% cases)
+  design$variables[idx, newvr] = TRUE
+
+  assign(getOption("surveytable.survey"), design, envir = .GlobalEnv)
+}
